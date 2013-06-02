29d3ed7b293d456c2b6a3eefdda7a336ce1578c5
Max Presman <max@presman.ca>
2013-06-02 12:19:03 -0400

    vagrant init 12.04

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+sudo su
+apt-get update
+apt-get upgrade --yes 
+apt-get install git curl python-pip zsh expect --yes
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install libmysqlclient-dev mysql-client mysql-server redis --yes
+
+
+sleep 5
+mysql -uroot -e <<EOSQL "UPDATE mysql.user SET Password=PASSWORD('sDk7N4CqcmMAMYyahEufMfk8cnV6huAR') WHERE User='root'; FLUSH PRIVILEGES;"
+EOSQL
+
+
+/etc/init.d/mysql stop
+/etc/init.d/mysql start
+
+#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+pip install virtualenv
+sudo vagrant
+cd /home/vagrant
+virtualenv venv
+
