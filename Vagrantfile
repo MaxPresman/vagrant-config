fb62af3e32e7987c79c27b3c473050ba62262fcb
Max Presman <max@presman.ca>
2014-03-02 23:22:54 -0800

    moving to 13.10

--- Vagrantfile
+++ Vagrantfile
@@ -7,14 +7,14 @@ Vagrant.configure("2") do |config|
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "12_04"
+  config.vm.box = "ubuntu"
 
   config.vm.provision :shell, :path => "bootstrap.sh"
-  config.vm.synced_folder "~/Desktop/vagrant_mount", "/home/vagrant"
+  config.vm.synced_folder "~/Desktop/vagrant_mount", "/etc/mounted"
 
   # The url from where the 'config.vm.box' box will be fetched if it
   # doesn't already exist on the user's system.
-  config.vm.box_url = "https://s3-us-west-2.amazonaws.com/squishy.vagrant-boxes/precise64_squishy_2013-02-09.box"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/saucy/current/saucy-server-cloudimg-amd64-vagrant-disk1.box"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,34 @@
 sudo su
 apt-get update
 apt-get upgrade --yes 
-apt-get install git curl python-pip zsh python-dev --yes
+apt-get install git curl python-pip zsh python-dev xorg --yes
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get install libmysqlclient-dev mysql-client mysql-server redis-server --yes
+apt-get install libmysqlclient-dev mysql-client mysql-server redis-server cmake --yes
+apt-get install language-pack-en --yes
+apt-get install openssl libssl-dev --yes
+apt-get install dist-upgrade --yes
 
-
+# update the mysql password
 sleep 5
 mysql -uroot -e <<EOSQL "UPDATE mysql.user SET Password=PASSWORD('sDk7N4CqcmMAMYyahEufMfk8cnV6huAR') WHERE User='root'; FLUSH PRIVILEGES;"
 EOSQL
 
-
 /etc/init.d/mysql stop
 /etc/init.d/mysql start
 
-#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
+# setup node
+. /root/.nvm/nvm.sh
+nvm install 0.8
+nvm install 0.10
+
+# add python packages
 pip install virtualenv
-#sudo vagrant
-#cd /home/vagrant
-#virtualenv venv
+
+apt-get autoremove
+apt-get clean
 
